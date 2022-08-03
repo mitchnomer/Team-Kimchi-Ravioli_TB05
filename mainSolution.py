c0727f184c13e6a16d894f7285dff8d6be9c3a04
Mitch <mpacheco.glvz@gmail.com>
2022-08-04 05:44:20 +0800

    first commit

---
+++ mainSolution.py
@@ -0,0 +1,24 @@
+import api, coh, overheads, profit_loss
+
+def main():
+    forex = api.api_function()
+    overheads_max = overheads.overhead_functions(forex)
+    pnl_loss = profit_loss.profitloss_function(forex)
+    coh_loss = coh.coh_function(forex)
+
+    with open('summary_report.txt', 'w') as f:
+        f.write(f'[REAL_TIME_CURRENCY_CONVERSION_RATE] USD1 = SGD{forex}\n')
+        f.write(f'[HIGHEST_OVERHEAD] {overheads_max[0].upper()}: SGD{overheads_max[1]}\n')
+
+        if coh_loss == []: #this means no loss occurred
+            f.write(f'[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
+        else:
+            for i in range(len(coh_loss)):
+                f.write(f'[CASH DEFICIT] DAY: {coh_loss[i][0]}, AMOUNT: {coh_loss[i][0]}\n')
+
+        if pnl_loss == []: #this means there were no losses
+            f.write(f'[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
+        else:
+            for i in range(len(pnl_loss)):
+                f.write(f'[PROFIT DEFICIT] DAY: {pnl_loss[i][0]}, AMOUNT: {pnl_loss[i][1]}\n')
+        
