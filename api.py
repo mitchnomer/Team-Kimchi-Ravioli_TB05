0f57e0155bf0a49d15c097fbcbdba05a5e27bf1c
Team Kimchi Raviolis TB05 Miguel Pacheco <S10247805@connect.np.edu.sg>
2022-08-04 23:40:37 +0800

    eleventh commit

--- api.py
+++ api.py
@@ -1,16 +1,13 @@
 import requests
 
 def api_function():
-    
-    function = 'CURRENCY_EXCHANGE_RATE'
-    from_symbol = 'USD'
-    to_symbol = 'SGD'
+    #this function is created to extract the currency exchange rate through the api.
+  
     api_key = 'PRIKISIIKARLHL8Z'
-
-    url= f'https://www.alphavantage.co/query?function={function}&from_currency={from_symbol}&to_currency={to_symbol}&apikey={api_key}'
-
-    data = requests.get(url).json()
-
-    return float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    url= f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+    response = requests.get(url).json()
+    
+    #return float is used to specifically retrieve the Realtime Currency Exchange Rate and Exchange Rate.
+    return float(response['Realtime Currency Exchange Rate']['5. Exchange Rate'])
 
 print(api_function())
--- mainSolution.py
+++ mainSolution.py
@@ -2,25 +2,25 @@ import api, coh, overheads, profit_loss
 
 def main():
     forex = api.api_function()
-    overheads_max = overheads.overhead_functions(forex)
+    overheads_max = overheads.overhead_function(forex)
     pnl_loss = profit_loss.profitloss_function(forex)
     coh_loss = coh.coh_function(forex)
 
     with open('summary_report.txt', 'w') as f:
-        f.write(f'[REAL_TIME_CURRENCY_CONVERSION_RATE] USD1 = SGD{forex}\n')
+        f.write(f'[CURRENCY_EXCHANGE_RATE] USD = SGD{forex}\n')
         f.write(f'[HIGHEST_OVERHEAD] {overheads_max[0].upper()}: SGD{overheads_max[1]}\n')
 
-        if coh_loss == []: #this means no loss occurred
-            f.write(f'[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
+        if coh_loss == []: #this means that no loss has occurred and that
+            f.write(f'[CASH EXCESS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
         else:
             for r in range(len(coh_loss)):
-                f.write(f'[CASH DEFICIT] DAY: {coh_loss[r][0]}, AMOUNT: {coh_loss[r][0]}\n')
+                f.write(f'[CASH LOSS] DAY: {coh_loss[r][0]}, AMOUNT: {coh_loss[r][0]}\n')
 
-        if pnl_loss == []: #this means there were no losses
-            f.write(f'[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
+        if pnl_loss == []: #this means no loss occured
+            f.write(f'[NET PROFIT EXCESS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
         else:
             for r in range(len(pnl_loss)):
-                f.write(f'[PROFIT DEFICIT] DAY: {pnl_loss[r][0]}, AMOUNT: {pnl_loss[r][1]}\n')
+                f.write(f'[PROFIT LOSS] DAY: {pnl_loss[r][0]}, AMOUNT: {pnl_loss[r][1]}\n')
     
     print("output is successful")
 
